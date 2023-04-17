5ae756d2223e1022eb959389e71d0b8bf30889ca
Ingenieria Sistemas31 <isi31@lab.uca.edu.ni>
2023-04-17 08:27:08 -0600

    ‘terminado’

---
+++ funciones.h
@@ -0,0 +1,8 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+void clasificar_angulo();
+void clasificar_triangulo();
+void calcular_area_rectangulo();
+
+#endif // FUNCIONES_H_INCLUDED
--- principal.cpp
+++ principal.cpp
@@ -1,4 +1,38 @@
-#include<iostream>
+#include <iostream>
+#include "funciones.h"
 
 using namespace std;
-void menu;
+
+int main() {
+    int opcion;
+    
+    do {
+        cout << "Menú de opciones:" << endl;
+        cout << "1. Identificar la clasificación de un ángulo según su medida." << endl;
+        cout << "2. Identificar la clasificación de un triángulo según la medida de sus lados." << endl;
+        cout << "3. Calcular el área de un rectángulo." << endl;
+        cout << "4. Salir." << endl;
+        cout << "Ingrese la opción deseada: ";
+        cin >> opcion;
+        
+        switch (opcion) {
+            case 1:
+                clasificar_angulo();
+                break;
+            case 2:
+                clasificar_triangulo();
+                break;
+            case 3:
+                calcular_area_rectangulo();
+                break;
+            case 4:
+                break;
+            default:
+                cout << "Opción no válida, intente de nuevo." << endl;
+                break;
+        }
+        
+    } while (opcion != 4);
+    
+    return 0;
+}
